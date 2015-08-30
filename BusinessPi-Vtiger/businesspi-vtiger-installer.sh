3eb902ff0ce8563dd10730eb41c50f856388e3b9
Salih Emin <cerebrux.gr@gmail.com>
2015-08-30 16:07:44 -0500

    Διόρθωση και Σχόλια
    Διορθώθηκε κρίσιμο bug στην εξαγωγή του Vtiger και προστέθηκαν ενημερωτικά στάδια

--- BusinessPi-Vtiger/businesspi-vtiger-installer.sh
+++ BusinessPi-Vtiger/businesspi-vtiger-installer.sh
@@ -1,15 +1,33 @@
 #!/bin/bash
+echo '#########################################'
+echo '# Αλλαγή το Hostname σε Vtiger και      #'
+echo '# domain σε vtiger.businesspi.local     #'
+echo '#########################################'
+sleep 3
 hostname vtiger
 service hostname restart
 cp /etc/hosts /etc/hosts.origin
 cp configuration-files/hosts /etc/hosts
-
+echo '#########################################'
+echo '# Ενημέρωση συστήματος και εγκατάσταση  #'
+echo '#               LAMP                    #'
+echo '#########################################'
+sleep 3
 apt-get update && apt-get -y upgrade && apt-get -y install apache2 libapache2-mod-php5 mysql-server php5-mysql php-apc php5-xmlrpc php-soap php5-gd php5-curl unzip apache2-utils libgd-tools
-
+echo '#########################################'
+echo '# Ρυθμίσεις ασφαλείας για την MySQL     #'
+echo '#                                       #'
+echo '#########################################'
+sleep 3
 mysql_secure_installation
-
+echo '#########################################'
+echo '# Λήψη και εγατάσταση Vtiger CRM 6.3.0  #'
+echo '#                                       #'
+echo '#########################################'
 wget http://sourceforge.net/projects/vtigercrm/files/vtiger%20CRM%206.3.0/Core%20Product/vtigercrm6.3.0.tar.gz
-tar -xvzf vtigercrm6.3.0.tar.gz /var/www/
+tar -xvzf vtigercrm6.3.0.tar.gz
+rm /var/www/index.html
+mv vtigercrm /var/www/vtigercrm
 chmod -R 775 /var/www/vtigercrm
 chown -R www-data:www-data /var/www/vtigercrm
 cp /etc/php5/apache2/php.ini /etc/php5/apache2/php.ini.origin
@@ -17,13 +35,23 @@ cp /etc/mysql/my.cnf /etc/mysql/my.cnf.origin
 cp configuration-files/php.ini /etc/php5/apache2/php.ini
 cp configuration-files/my.cnf /etc/mysql/my.cnf
 cp configuration-files/vtigercrmapache /etc/apache2/sites-available/
-
 ln -s /etc/apache2/sites-available/vtigercrmapache /etc/apache2/sites-enabled/030-vtiger.conf
-
+echo '#########################################'
+echo '# Απανεκκίνηση Apache και MySQL         #'
+echo '#                                       #'
+echo '#########################################'
+sleep 3
 service	apache restart
+service mysql restart
 
 echo ''
 echo ''
 echo ''
+echo '#########################################'
+echo '# H εγκατάσταση Ολοκληρώθηκε            #'
+echo '#                                       #'
+echo '#########################################'
 echo ''
 echo 'Τώρα θα πρέπει να ρυθμίσετε τον υπολογιστή σας στο host αρχείο του να μεταφράζει το http://vtiger.businesspi.local στην IP που έχει λάβει (ή θέσατε εσείς) για να να συνδεθείτε στο Vtiger'
+echo ''
+echo ''
